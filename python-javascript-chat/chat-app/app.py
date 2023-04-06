a9d542790945931427b05977e9ee3e3cfb2cc5c3
Zadeson <57639667+Zadeson@users.noreply.github.com>
2023-04-05 21:00:50 -0500

    Big Update

--- python-javascript-chat/chat-app/app.py
+++ python-javascript-chat/chat-app/app.py
@@ -20,7 +20,9 @@ def chat():
 @socketio.on('join_room')
 def handle_join_room_event(data):
     join_room(data['room'])
-    send({'msg': data['username'] + " has joined the room."}, room=data['room'])
+    socketio.emit("message", {"username": data["username"], "msg": "has joined the room.", "isServerMessage": True}, room=data["room"])
+
+
 
 @socketio.on('leave_room')
 def handle_leave_room_event(data):
@@ -29,7 +31,39 @@ def handle_leave_room_event(data):
 
 @socketio.on('send_message')
 def handle_send_message_event(data):
-    send({'msg': data['username'] + ": " + data['msg']}, room=data['room'])
+    socketio.emit("message", {"username": data["username"], "msg": data["msg"], "isServerMessage": False}, room=data["room"])
+
+
+@socketio.on('join_room')
+def handle_join_room_event(data):
+    join_room(data['room'])
+    send({'msg': data['username'] + " has joined the room.", 'isServerMessage': True}, room=data['room'])
+
+@socketio.on('leave_room')
+def handle_leave_room_event(data):
+    leave_room(data['room'])
+    send({'msg': data['username'] + " has left the room.", 'isServerMessage': True}, room=data['room'])
+
+room_messages = {} # Add this line to store messages for each room
+
+@socketio.on('join_room')
+def handle_join_room_event(data):
+    join_room(data['room'])
+    socketio.emit("message", {"username": data["username"], "msg": "has joined the room.", "isServerMessage": True}, room=data["room"])
+
+    if data['room'] in room_messages:  # Add this block
+        for msg in room_messages[data['room']]:
+            send(msg, room=request.sid)  # Send past messages only to the new user, using request.sid
+            
+@socketio.on('send_message')
+def handle_send_message_event(data):
+    msg = {'username': data['username'], 'msg': data['msg']}
+    if data['room'] not in room_messages: # Add this block
+        room_messages[data['room']] = []
+    room_messages[data['room']].append(msg)
+    send(msg, room=data['room'])
+
+
 
 if __name__ == '__main__':
     socketio.run(app)
--- python-javascript-chat/chat-app/frontend/src/App.js
+++ python-javascript-chat/chat-app/frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import io from 'socket.io-client';
 import styles from './App.module.css'; // Import the CSS file
+import { FaUserPlus, FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const socket = io('http://localhost:5000');
@@ -12,29 +13,41 @@ function App() {
   const [room, setRoom] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
+  const [currentRoom, setCurrentRoom] = useState('');
 
   const login = () => {
     // Add your authentication logic here
     setLoggedIn(true);
   };
 
-  const joinRoom = () => {
-    if (room !== '') {
-      socket.emit('join_room', { username, room });
 
-      socket.on('message', (data) => {
-        setMessages((oldMessages) => [...oldMessages, data.msg]);
+  const joinRoom = () => {
+    if (room !== "") {
+      setMessages([]);
+      setCurrentRoom(room);
+      socket.emit("join_room", { username, room });
+  
+      socket.on("message", (data) => {
+        setMessages((oldMessages) => [
+          ...oldMessages,
+          {
+            username: data.username,
+            message: data.msg,
+            timestamp: new Date().toLocaleTimeString(),
+            isServerMessage: data.isServerMessage,
+          },
+        ]);
       });
     }
   };
-
+  
+  
   const sendMessage = () => {
     if (message !== '') {
       socket.emit('send_message', { room, username, msg: message });
       setMessage('');
     }
   };
-
   return (
     <>
       {!loggedIn ? (
@@ -60,12 +73,21 @@ function App() {
             </div>
             <div className="form-group">
             <button
-              className={`btn ${styles.btn}`}
+              className={`btn ${
+                !username || !password ? styles.btnDisabled : styles.btnActive
+              }`}
               onClick={login}
               disabled={!username || !password}
+              style={
+                !username || !password
+                  ? { borderColor: "#575a66", color: "#ffffff" }
+                  : { borderColor: "#868895", color: "#ffffff" }
+              }
             >
               Login
             </button>
+
+
           </div>
 
           </div>
@@ -82,21 +104,47 @@ function App() {
                 onChange={(e) => setRoom(e.target.value)}
               />
             </div>
-            <button className={`btn ${styles.btn}`} onClick={joinRoom}>
-              Join Room
+            <button
+              className={`btn ${room === "" ? styles.btnDisabled : styles.btnActive}`}
+              onClick={joinRoom}
+              disabled={room === ""}
+              style={
+                room === ""
+                  ? { borderColor: "#575a66", color: "#ffffff" }
+                  : { borderColor: "#868895", color: "#ffffff" }
+              }
+            >
+              <FaUserPlus /> Join Room
             </button>
-            <div className="list-group mt-3">
-              {/* Your list of chat rooms */}
-            </div>
-
           </div>
           <div className={styles.chat}>
+            <div className={styles.chatHeader}>
+              <h3>{currentRoom ? currentRoom : 'No Room Selected'}</h3>
+            </div>
             <div className={styles.chatBox}>
-              {messages.map((message, index) => (
-                <div key={index}>
-                  <strong>{message.username}</strong>: {message.message}
+            {messages.map((message, index) => (
+              <div key={index} className={styles.messageContainer}>
+                <div>
+                  <span className={styles.timestamp}>{message.timestamp}</span>
+                  <br />
+                  {message.isServerMessage ? (
+                    <span>
+                      {message.username} has joined the room.
+                    </span>
+                  ) : (
+                    <>
+                      <strong>{'<' + message.username + '>'}</strong>
+                      <span> : </span>
+                      <span>{message.message}</span>
+                    </>
+                  )}
                 </div>
-              ))}
+              </div>
+            ))}
+
+
+
+
             </div>
             <div className="input-group">
               <input
@@ -107,16 +155,41 @@ function App() {
                 onChange={(e) => setMessage(e.target.value)}
               />
               <div className="input-group-append">
-                <button
-                  className={`btn ${styles.btn}`}
-                  type="button"
-                  onClick={sendMessage}
-                >
-                  Send
-                </button>
+              <button
+                className={`btn ${message === '' ? styles.btnDisabled : styles.btnActive}`}
+                type="button"
+                onClick={sendMessage}
+                disabled={message === ''}
+                style={
+                  message === ''
+                    ? { borderColor: "#575a66", color: "#ffffff" }
+                    : { borderColor: "#868895", color: "#ffffff" }
+                }
+              >
+                Send
+              </button>
+
+
+
               </div>
             </div>
           </div>
+          <button
+            className={`btn ${styles.btn}`}
+            style={{
+              position: "absolute",
+              bottom: "1rem",
+              left: "1rem",
+              borderColor: "#575a66",
+              color: "#ffffff"
+            }}
+            onClick={() => setLoggedIn(false)}
+          >
+            <FaSignOutAlt /> Log Out
+          </button>
+
+
+
         </div>
       )}
     </>
